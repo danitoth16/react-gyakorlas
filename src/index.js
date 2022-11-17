b9a509a0d5f2bb392e000ace2544bebb8ce6fabd
Toth Dani <dani.toth13@gmail.com>
2022-11-17 12:13:06 +0100

    egy újjabb komment

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ root.render(
 
 //csak kommenmt, hogy új commit legyen
 
+
+//egy újjabb komment 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
